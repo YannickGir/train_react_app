78cd07c2d86079d132facb5cf8fe188ce34e9bbe
Yannick <girofare@hotmail.com>
2023-12-18 22:35:52 +0100

    added navigation

--- train_react_app/src/App.js
+++ train_react_app/src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+
+
+
 function App() {
+
+    const Router = createBrowserRouter ([
+        {
+            path : '/',
+            element:
+            <div>
+                <h2>Accueil</h2>
+                <button style={{padding:'8px', margin:'8px'}}> 
+                    <Link to={'/Page1'} >Page1</Link>
+                </button> 
+                <button style={{padding:'8px', margin:'8px'}}>    
+                    <Link to={'/Page2'} >Page2</Link>
+                </button> 
+            </div>
+        },
+        {
+            path : '/Page1',
+            element:
+            <div>
+                <h2>Page 1</h2>
+                <button style={{padding:'8px', margin:'8px'}}>  
+                    <Link to={'/'} >Retour à l'Accueil</Link>
+                </button>
+            </div>
+        },
+        {
+            path : '/Page2',
+            element:
+            <div>
+            <h2>Page 2</h2>
+            <button style={{padding:'8px', margin:'8px'}}>  
+                <Link to={'/'} >Retour à l'Accueil</Link>
+            </button>
+        </div>
+        }
+
+    ])
+      
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <RouterProvider router={Router}/>
     </div>
   );
 }
