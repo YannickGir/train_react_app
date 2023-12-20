6f8abb685046c48ef826610069ddba685b2ef136
Yannick <girofare@hotmail.com>
2023-12-20 23:10:26 +0100

    installed mongodb

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
 const user_routes = require('./routes/user_routes')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
+app.use(cors())
+mongoose.connect('')
 app.use(user_routes)
