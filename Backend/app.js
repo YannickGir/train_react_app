43d809bbdf94bae908939667c794e6ad9e85aef2
Yannick <girofare@hotmail.com>
2023-12-20 23:29:40 +0100

    installed axios

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,17 @@ const app = express()
 const user_routes = require('./routes/user_routes')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const UserModel = require('./models/Users')
+
+require('dotenv').config();
 
 app.use(cors())
-mongoose.connect('')
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster0.rhyjx.mongodb.net/?retryWrites=true&w=majority`)
+
+app.get('/getUsers', (req, res)=>{
+    UserModel.find()
+    .then(users => res.json(users))
+    .catch(err =>res.json(err))
+})
+
 app.use(user_routes)
--- train_react_app/src/pages/Page2.jsx
+++ train_react_app/src/pages/Page2.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import { useEffect, useState }from 'react'
 import { useParams } from 'react-router-dom'
-
+import axios from 'axios'
 
 export default function Page1() {
     const {id} = useParams()
+
+    useEffect(()=>{
+        axios.get('')
+    }, [])
   return (
     <div>
         <h1> Page2 </h1>
         <p> My id : {id} </p>
+    
         
     </div>
   )
