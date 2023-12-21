e3582356beb28072d5545aaad1041318c138c155
Yannick <girofare@hotmail.com>
2023-12-21 08:14:34 +0100

    installed some modules

--- Backend/app.js
+++ Backend/app.js
@@ -8,12 +8,13 @@ const UserModel = require('./models/Users')
 require('dotenv').config();
 
 app.use(cors())
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster0.rhyjx.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster1.qzxbj9m.mongodb.net/test_react`, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/getUsers', (req, res)=>{
-    UserModel.find()
-    .then(users => res.json(users))
-    .catch(err =>res.json(err))
-})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB :'));
+db.once('open', () => console.log('Connexion à MongoDB réussie !'));
+
+
+
 
 app.use(user_routes)
--- Backend/routes/user_routes.js
+++ Backend/routes/user_routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 
+router.get('/getUsers', (req, res)=>{
+    UserModel.find()
+    .then(users => res.json(users))
+    .catch(err =>res.json(err))
+})
 
-
-
+module.exports = router;
