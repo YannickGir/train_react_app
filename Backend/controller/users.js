4cca3e21496d8d8975d697566e79a6ea576ef883
Yannick <girofare@hotmail.com>
2024-01-04 15:29:05 +0100

    added findOne to check if email does exists

--- Backend/controller/users.js
+++ Backend/controller/users.js
@@ -5,31 +5,31 @@ const UserModel = require("../models/Users");
 const addUser = async (req, res) =>{
     
     try {
-        db.users.findOne({ email: req.body.email }, (err, user) => {
-            if (err) {
-              console.error('Erreur lors de la recherche de l\'e-mail :', err);
-            }
-          
+        const user = await UserModel.findOne({ email: req.body.email }) 
             if (user) {
               console.log(`L'e-mail ${req.body.email} existe dans la collection.`);
-              // Faire quelque chose si l'e-mail existe
+              res.status(409).json({ error: 'Cet email est déjà utilisé. Veuillez choisir un autre.' });
+
             } else {
               console.log(`L'e-mail ${emailToCheck} n'existe pas dans la collection.`);
-              // Faire quelque chose si l'e-mail n'existe pas
-            }
-          });
-        const values = 
+               const values = 
             [
                 req.body.name,
                 req.body.email,
             ]
-        const newUser = await UserModel.create({
-            name: values[0],
-            email: values[1],
-        })
-        res.status(200).json({ message: "L'utilisateur a été créé avec succès !" });
+            const createUser = async () => {
+                await UserModel.create({
+                name: values[0],
+                email: values[1],
+                })
+            } 
+            createUser()
+            res.status(200).json({ message: "L'utilisateur a été créé avec succès !" });
         
-        console.log('création utilisateur');
+            console.log('création utilisateur');
+            
+          };
+       
     } catch (error) {
         console.log((error));
         res.status(500).json({error:'internal server Error'});
