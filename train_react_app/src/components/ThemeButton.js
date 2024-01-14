391af93d6145b8526d18ed5fdb2de5114c1a7d3a
Yannick <girofare@hotmail.com>
2024-01-14 19:02:22 +0100

    adjusted toogle button

--- train_react_app/src/components/ThemeButton.js
+++ train_react_app/src/components/ThemeButton.js
@@ -40,11 +40,12 @@ import useTheme from '../contexts/theme.context';
                 checked={themeMode === 'dark'}
             />
             <div className="w-11 h-6 bg-blue-400 rounded-full p-1 peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 transition-transform duration-300 ease-in-out transform">
-                <div
-                    className={`h-4 w-4 bg-white rounded-full shadow-md transform ${
-                        themeMode === 'dark' ? 'translate-x-5' : 'translate-x-0.5'
-                    } transition-transform duration-5000 ease-in-out`}
-                ></div>
+            <div
+
+className={`h-4 w-4 rounded-full shadow-md transform absolute ${
+    themeMode === 'dark' ? 'right-1 bg-black' : 'left-1 bg-white'
+} transition-transform duration-5000 ease-in-out`}
+></div>
             </div>
         </label>
 
