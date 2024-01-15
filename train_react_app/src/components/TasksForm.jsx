fc5e8f70eff5f43aaaa4f9995f9e49563550735c
Yannick <girofare@hotmail.com>
2024-01-15 12:27:42 +0100

    form without useState, using only html functions to set datasForm

--- train_react_app/src/components/TasksForm.jsx
+++ train_react_app/src/components/TasksForm.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 
 const TasksForm = ()=> {
+const handleSubmit = (e)=> {
+    e.preventDefault();
+    const form = e.target;
+    const elements = form.elements;
+    const task = elements.entryTask.value;
+    console.log(elements);
+    alert(`task :${task}`)
 
+}
     return (
         <div className='wrapper' >
-            <div style={{background:'gray', height:'400px', width:'400px'}}>
-              <form >
-                <label> Entrez une tâche ici : </label> <br/>
-                <input style={{color: 'black'}}/><br/>
-                <button>Ajouter une tâche</button>
+            <div style={{background:'gray', height:'400px', width:'500px'}}>
+              <form onSubmit={handleSubmit} >
+                <label htmlFor='entryTask'> Entrez une tâche ici : </label> <br/>
+                <input type='text' id='entryTask' style={{color: 'black'}}/><br/>
+                <button type='submit' className='customButton'>Ajouter une tâche</button>
 
                 <div style={{marginTop:'20px'}}>
                     <label> Liste de vos tâches : </label> <br/>
-                    <input style={{color: 'black'}}/> <button>Modifier</button><button style={{marginLeft:'15px'}}>Supprimer</button>
+                    <input style={{color: 'black'}}/> <button className='customButton'>Modifier</button><button className='customButton' >Supprimer</button>
                 </div>
               
 
