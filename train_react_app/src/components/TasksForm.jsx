f64fab629cf34bd515a204edc29ab2bdf226f775
Yannick <girofare@hotmail.com>
2024-01-15 13:30:39 +0100

    added handleSubmit and mapping the list of tasks

--- train_react_app/src/components/TasksForm.jsx
+++ train_react_app/src/components/TasksForm.jsx
@@ -1,36 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const TasksForm = ()=> {
+const [taskList, setTaskList] = useState([])
+
+    const addTaskToList = (task)=> {
+        setTaskList((previousTaskList) => {
+            const newTaskList = [...previousTaskList, task];
+            // alert(`taskList : ${newTaskList}`);
+            setTaskList(newTaskList)
+            return newTaskList;
+        })
+    }
+
 const handleSubmit = (e)=> {
     e.preventDefault();
     const form = e.target;
     const elements = form.elements;
     const task = elements.entryTask.value;
     console.log(elements);
-   
+   addTaskToList(task)
     form.reset(); 
-    
-    alert(`task :${task}`)
-
 }
     return (
         <div className='wrapper' >
             <div style={{background:'gray', height:'400px', width:'500px'}}>
               <form onSubmit={handleSubmit} >
-                <label htmlFor='entryTask'> Entrez une tâche ici : </label> <br/>
-                <input type='text' id='entryTask' style={{color: 'black'}}/><br/>
-                <button type='submit' className='customButton'>Ajouter une tâche</button>
+                    <label htmlFor='entryTask'> Entrez une tâche ici : </label> <br/>
+                    <input type='text' id='entryTask' style={{color: 'black'}}/><br/>
+                    <button type='submit' className='customButton'>Ajouter une tâche</button>
 
-                <div style={{marginTop:'20px'}}>
-                    <label> Liste de vos tâches : </label> <br/>
-                    <input style={{color: 'black'}}/> <button className='customButton'>Modifier</button><button className='customButton' >Supprimer</button>
-                </div>
+                    <div style={{marginTop:'20px'}}>
+                        <label> Liste de vos tâches : </label> <br/>
+                        {taskList.map((task, index)=>
+                                (
+                                        <div key={index}>
+                                            <input value={task} style={{color: 'black'}}/> 
+                                            <button className='customButton'>Modifier</button>
+                                            <button className='customButton' >Supprimer</button>
+                                        </div>
+                                )
+                            )   
+                        }
+                    </div>
               
 
-            </form>  
+                </form>  
             </div>
-            
-
         </div>
     )
 }
