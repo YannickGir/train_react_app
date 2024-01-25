17ada5cf4e3c4149a6993b1f92e3185cb0b49000
Yannick <girofare@hotmail.com>
2024-01-25 18:59:58 +0100

    implemented carousel example

--- train_react_app/src/components/Dashboard.jsx
+++ train_react_app/src/components/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, { useState, useRef, useEffect } from "react";
 import "../styles/dashboard.css";
 import axios from "axios";
 
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 const Dashboard = (props) => {
 
 const [imagesData, setImagesData] = useState([])
@@ -171,6 +173,42 @@ const handleDeleteImage = async (e)=> {
         console.error('Erreur lors de la suppression de l\'image :', error);
     }
 }
+
+
+const CARDS = 10;
+const MAX_VISIBILITY = 3;
+
+const Card = ({title, content}) => (
+  <div className='card'>
+    <h2>{title}</h2>
+    <p>{content}</p>
+  </div>
+);
+
+const Carousel = ({children}) => {
+  const [active, setActive] = useState(2);
+  const count = React.Children.count(children);
+  
+  return (
+    <div className='carousel'>
+      {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><FaChevronLeft/></button>}
+      {React.Children.map(children, (child, i) => (
+        <div className='card-container' style={{
+            '--active': i === active ? 1 : 0,
+            '--offset': (active - i) / 3,
+            '--direction': Math.sign(active - i),
+            '--abs-offset': Math.abs(active - i) / 3,
+            'pointer-events': active === i ? 'auto' : 'none',
+            'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+            'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+          }}>
+          {child}
+        </div>
+      ))}
+      {active < count - 1 && <button className='nav right' onClick={() => setActive(i => i + 1)}><FaChevronRight/></button>}
+    </div>
+  );
+};
   
   return (
     <div className="dashboardWrapper" style={{ color: "black" }}>
@@ -209,7 +247,16 @@ const handleDeleteImage = async (e)=> {
             <button key={index} onClick={()=> handleDeleteImage(index)}>Supprimer</button>
         </div>
       )})}
-     
+     <div className='app'>
+        <div className="gallery-wrapper">
+    <Carousel>
+      {[...new Array(CARDS)].map((_, i) => (
+        <Card title={'Card ' + (i + 1)} content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
+      ))}
+    </Carousel>
+        </div>
+   
+  </div>
     </div>
   );
 };
