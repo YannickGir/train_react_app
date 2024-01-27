aee9943fa9ca2c81d92b035153f33680a7474b29
Yannick <girofare@hotmail.com>
2024-01-27 10:27:02 +0100

    added placeholder and list only when I search someting

--- train_react_app/src/components/Selector.jsx
+++ train_react_app/src/components/Selector.jsx
@@ -1,51 +1,37 @@
-import { Fragment, useEffect, useState } from 'react'
-import { Combobox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+// Selector.jsx
+import React, { Fragment, useState } from 'react';
+import { Combobox, Transition } from '@headlessui/react';
+import { ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
-import {Country, State} from "country-state-city"
-
-// const data = [
-//   { id: 1, name: 'Wade Cooper' },
-//   { id: 2, name: 'Arlene Mccoy' },
-//   { id: 3, name: 'Devon Webb' },
-//   { id: 4, name: 'Tom Cook' },
-//   { id: 5, name: 'Tanya Fox' },
-//   { id: 6, name: 'Hellen Schmidt' },
-// ]
-
-const Selector = ({data, selected, setSelected, onSelectCity }) => {
-
-  const [query, setQuery] = useState('')
+const Selector = ({ data, selected, setSelected, onSelectCity }) => {
+  const [query, setQuery] = useState('');
 
   const filteredSelected =
     query === ''
-      ? data
+      ? []
       : data.filter((selection) =>
           selection.name
             .toLowerCase()
             .replace(/\s+/g, '')
             .includes(query.toLowerCase().replace(/\s+/g, ''))
-        )
+        );
 
   return (
     <div className="w-72">
-      <Combobox 
-      value={selected} 
-      onChange={(selected) => {
-        console.log('Selected:', selected);
-        setSelected(selected);
-     }}
+      <Combobox
+        value={selected}
+        onChange={(selected) => {
+          console.log('Selected:', selected);
+          setSelected(selected);
+        }}
       >
         <div className="relative mt-1">
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-black text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-black focus:ring-0 custom-selector-input"
-              displayValue={(person) => (person && person.name) || ''}
+              displayValue={(item) => (item && item.name) || ''}
               onChange={(event) => setQuery(event.target.value)}
-              onSelect={() => {
-                console.log('Selected:', selected);
-                onSelectCity && onSelectCity(selected);
-              }}
+              placeholder="Choisir..."
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
@@ -67,17 +53,19 @@ const Selector = ({data, selected, setSelected, onSelectCity }) => {
                   Nothing found.
                 </div>
               ) : (
-                filteredSelected.map((selection) => (
+                filteredSelected.map((item) => (
                   <Combobox.Option
-                    key={selection.id}
+                    key={item.id}
+                    value={item}
                     className={({ active }) =>
                       `relative cursor-default select-none py-2 pl-10 pr-4 ${
                         active ? 'bg-teal-600 text-black' : 'text-gray-900'
                       }`
                     }
-                    value={selection}
-        
-                     
+                    onSelect={() => {
+                      setSelected(item);
+                      onSelectCity && onSelectCity(item);
+                    }}
                   >
                     {({ selected, active }) => (
                       <>
@@ -86,16 +74,14 @@ const Selector = ({data, selected, setSelected, onSelectCity }) => {
                             selected ? 'font-medium' : 'font-normal'
                           }`}
                         >
-                          {selection.name}
+                          {item.name}
                         </span>
                         {selected ? (
                           <span
                             className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
                               active ? 'text-black' : 'text-teal-600'
                             }`}
-                          >
-                            {/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
-                          </span>
+                          />
                         ) : null}
                       </>
                     )}
@@ -107,8 +93,7 @@ const Selector = ({data, selected, setSelected, onSelectCity }) => {
         </div>
       </Combobox>
     </div>
-  )
-}
+  );
+};
 
 export default Selector;
-
